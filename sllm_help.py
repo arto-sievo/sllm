648503ab141779be434ee19461bb83f64a7cfd87
Arto Pihlaja <arto.pihlaja@gmail.com>
2023-06-11 12:18:42 +0000

    key vault for web app

--- sllm_help.py
+++ sllm_help.py
@@ -1,10 +1,21 @@
-import configparser
-conf = configparser.ConfigParser()
-conf.read('config.ini')
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+# https://learn.microsoft.com/en-us/azure/key-vault/secrets/quick-create-python?tabs=azure-cli
+keyVaultName = 'kv-artodev'
+kvUrl = f'https://{keyVaultName}.vault.azure.net'
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=kvUrl, credential=credential)
+OPENAI_API_KEY=client.get_secret('openai-api-key')
+PC_API_KEY=client.get_secret('pinecone-api-key')
+PC_ENV=client.get_secret('pinecone-environment')
 
-OPENAI_API_KEY=conf['openai']['api_key']  
-PC_API_KEY=conf['pinecone']['api_key']  
-PC_ENV=conf['pinecone']['environment'] 
+# import configparser
+# conf = configparser.ConfigParser()
+# conf.read('config.ini')
+
+# OPENAI_API_KEY=conf['openai']['api_key']  
+# PC_API_KEY=conf['pinecone']['api_key']  
+# PC_ENV=conf['pinecone']['environment'] 
 
 
 from langchain.document_loaders import TextLoader
